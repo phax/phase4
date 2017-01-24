73602a86e90d0b9b12f71c4c82ce11441816c719
Martin Bayerl <maximilianmartin@gmx.at>
2017-01-24 13:59:38 +0100

    Some client changes

--- ph-as4-lib/src/main/java/com/helger/as4/client/AS4Client.java
+++ ph-as4-lib/src/main/java/com/helger/as4/client/AS4Client.java
@@ -2,15 +2,21 @@ package com.helger.as4.client;
 
 import java.io.File;
 
-import javax.annotation.Nullable;
+import javax.mail.internet.MimeMessage;
 
 import org.w3c.dom.Document;
 import org.w3c.dom.Node;
 
 import com.helger.as4.attachment.WSS4JAttachment;
+import com.helger.as4.crypto.ECryptoAlgorithmSign;
+import com.helger.as4.crypto.ECryptoAlgorithmSignDigest;
 import com.helger.as4.messaging.domain.AS4UserMessage;
 import com.helger.as4.messaging.domain.CreateUserMessage;
+import com.helger.as4.messaging.encrypt.EncryptionCreator;
+import com.helger.as4.messaging.mime.MimeMessageCreator;
+import com.helger.as4.messaging.sign.SignedMessageCreator;
 import com.helger.as4.soap.ESOAPVersion;
+import com.helger.as4.util.AS4ResourceManager;
 import com.helger.as4lib.ebms3header.Ebms3CollaborationInfo;
 import com.helger.as4lib.ebms3header.Ebms3MessageInfo;
 import com.helger.as4lib.ebms3header.Ebms3MessageProperties;
@@ -30,6 +36,8 @@ import com.helger.commons.string.StringHelper;
 @WorkInProgress
 public class AS4Client
 {
+  private final AS4ResourceManager aResMgr = new AS4ResourceManager ();
+
   private ICommonsList <WSS4JAttachment> aAttachments = new CommonsArrayList <> ();
   private Node aPayload;
   private ESOAPVersion eSOAPVersion;
@@ -37,10 +45,14 @@ public class AS4Client
   // Keystore attributes
   // TODO look at AS2 Client / ClientSettinggs /ClientRequest
   private File m_aKeyStoreFile;
+  private String m_sKeyStoreAlias;
   private String m_sKeyStorePassword;
+  // org.apache.wss4j.common.crypto.Merlin is the default value
+  private String m_sKeyStoreProvider = "org.apache.wss4j.common.crypto.Merlin";
 
   // Document related attributes
   private Document aDoc;
+  private MimeMessage aMsg;
   private ICommonsList <Ebms3Property> aEbms3Properties = new CommonsArrayList <> ();
   // For Message Info
   private String sMessageIDPrefix;
@@ -63,18 +75,79 @@ public class AS4Client
   private String sToRole;
   private String sToPartyID;
 
+  // Signing additional attributes
+  private ECryptoAlgorithmSign eECryptoAlgorithmSign = ECryptoAlgorithmSign.SIGN_ALGORITHM_DEFAULT;
+  private ECryptoAlgorithmSignDigest eECryptoAlgorithmSignDigest = ECryptoAlgorithmSignDigest.SIGN_DIGEST_ALGORITHM_DEFAULT;
+
   /**
-   * @return The encrypted Document or null if something failed.
+   * This method encrypts the current Document and is producing a encrypted
+   * Document or a MimeMessage. Encrypted Document if only a SOAP BodyPaylod is
+   * present MimeMessage if Attachments are present
+   *
+   * @throws Exception
+   *         if something goes wrong in the encryption process
    */
-  @Nullable
-  public Document encryptDocument ()
+  public void encryptDocument () throws Exception
   {
-    if (StringHelper.hasNoText (m_sKeyStorePassword))
+    if (aDoc == null)
     {
+      throw new IllegalStateException ("No Document is set.");
+    }
+    else
+    {
+      _checkKeystoreAttributes ();
+
+      final EncryptionCreator aEncCreator = new EncryptionCreator ();
+      // MustUnderstand always set to true
+      if (aAttachments.isNotEmpty ())
+      {
+        aMsg = aEncCreator.encryptMimeMessage (eSOAPVersion, aDoc, true, aAttachments, aResMgr);
+      }
+      else
+      {
+        aDoc = aEncCreator.encryptSoapBodyPayload (eSOAPVersion, aDoc, true);
+      }
+    }
+  }
+
+  public void signDocument () throws Exception
+  {
+    if (aDoc == null)
+    {
+      throw new IllegalStateException ("No Document is set.");
+    }
+    else
+    {
+      _checkKeystoreAttributes ();
+      aDoc = new SignedMessageCreator ().createSignedMessage (aDoc,
+                                                              eSOAPVersion,
+                                                              aAttachments,
+                                                              aResMgr,
+                                                              true,
+                                                              eECryptoAlgorithmSign,
+                                                              eECryptoAlgorithmSignDigest);
+      if (aAttachments.isNotEmpty ())
+      {
+        aMsg = new MimeMessageCreator (eSOAPVersion).generateMimeMessage (aDoc, aAttachments);
+      }
 
     }
+  }
 
-    return null;
+  private void _checkKeystoreAttributes ()
+  {
+    if (StringHelper.hasNoText (m_sKeyStoreAlias) ||
+        StringHelper.hasNoText (m_sKeyStorePassword) ||
+        !m_aKeyStoreFile.exists ())
+    {
+      throw new IllegalStateException ("At least one of the following Alias: " +
+                                       m_sKeyStoreAlias +
+                                       ", Password: " +
+                                       m_sKeyStorePassword +
+                                       " or the KeyStoreFile:(rue if the file exists) " +
+                                       m_aKeyStoreFile.exists () +
+                                       "are not set.");
+    }
   }
 
   /**
@@ -150,6 +223,16 @@ public class AS4Client
     this.m_aKeyStoreFile = m_aKeyStoreFile;
   }
 
+  public String getKeyStoreAlias ()
+  {
+    return m_sKeyStoreAlias;
+  }
+
+  public void setKeyStoreAlias (final String m_sKeyStoreAlias)
+  {
+    this.m_sKeyStoreAlias = m_sKeyStoreAlias;
+  }
+
   public String getKeyStorePassword ()
   {
     return m_sKeyStorePassword;
@@ -158,6 +241,16 @@ public class AS4Client
   public void setKeyStorePassword (final String m_sKeyStorePassword)
   {
     this.m_sKeyStorePassword = m_sKeyStorePassword;
+  }
+
+  public String getKeyStoreProvider ()
+  {
+    return m_sKeyStoreProvider;
+  }
+
+  public void setKeyStoreProvider (final String m_sKeyStoreAlias)
+  {
+    this.m_sKeyStoreProvider = m_sKeyStoreAlias;
   }
 
   public Document getDoc ()
@@ -288,6 +381,26 @@ public class AS4Client
   public void setToPartyID (final String sToPartyID)
   {
     this.sToPartyID = sToPartyID;
+  }
+
+  public ECryptoAlgorithmSign getECryptoAlgorithmSign ()
+  {
+    return eECryptoAlgorithmSign;
+  }
+
+  public void setECryptoAlgorithmSign (final ECryptoAlgorithmSign eECryptoAlgorithmSign)
+  {
+    this.eECryptoAlgorithmSign = eECryptoAlgorithmSign;
+  }
+
+  public ECryptoAlgorithmSignDigest getECryptoAlgorithmSignDigest ()
+  {
+    return eECryptoAlgorithmSignDigest;
+  }
+
+  public void seeECryptoAlgorithmSignDigest (final ECryptoAlgorithmSignDigest eECryptoAlgorithmSignDigest)
+  {
+    this.eECryptoAlgorithmSignDigest = eECryptoAlgorithmSignDigest;
   }
 
 }
