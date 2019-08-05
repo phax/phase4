d406f187495300a1a07ddee2107fd66df3fff84a
phax <philip@helger.com>
2019-08-05 09:04:04 +0200

    Removed check for mandatory properties

--- ph-as4-lib/src/main/java/com/helger/as4/client/AS4ClientUserMessage.java
+++ ph-as4-lib/src/main/java/com/helger/as4/client/AS4ClientUserMessage.java
@@ -153,10 +153,13 @@ public class AS4ClientUserMessage extends AbstractAS4Client
     if (StringHelper.hasNoText (m_sToPartyID))
       throw new IllegalStateException ("ToPartyID needs to be set");
 
-    if (m_aEbms3Properties.containsNone (x -> x.getName ().equals (CAS4.ORIGINAL_SENDER)))
-      throw new IllegalStateException ("Mandatory property originalSender is missing");
-    if (m_aEbms3Properties.containsNone (x -> x.getName ().equals (CAS4.FINAL_RECIPIENT)))
-      throw new IllegalStateException ("Mandatory property finalRecipient is missing");
+    if (false)
+    {
+      if (m_aEbms3Properties.containsNone (x -> x.getName ().equals (CAS4.ORIGINAL_SENDER)))
+        throw new IllegalStateException ("Mandatory property originalSender is missing");
+      if (m_aEbms3Properties.containsNone (x -> x.getName ().equals (CAS4.FINAL_RECIPIENT)))
+        throw new IllegalStateException ("Mandatory property finalRecipient is missing");
+    }
   }
 
   @Override
