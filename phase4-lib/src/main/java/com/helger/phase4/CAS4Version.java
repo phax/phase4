074d43d5852eed0f7899b87ddda4405355389620
Philip Helger <philip@helger.com>
2019-11-12 17:39:34 +0100

    Added version number as constant

---
+++ phase4-lib/src/main/java/com/helger/phase4/CAS4Version.java
@@ -0,0 +1,69 @@
+/**
+ * Copyright (C) 2015-2019 Philip Helger (www.helger.com)
+ * philip[at]helger[dot]com
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *         http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.helger.phase4;
+
+import javax.annotation.concurrent.Immutable;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import com.helger.commons.collection.impl.ICommonsMap;
+import com.helger.commons.io.resource.ClassPathResource;
+import com.helger.commons.lang.PropertiesHelper;
+
+/**
+ * Contains application wide constants.
+ *
+ * @author Philip Helger
+ */
+@Immutable
+public final class CAS4Version
+{
+  /** Current version - from properties file */
+  public static final String BUILD_VERSION;
+  /** Build timestamp - from properties file */
+  public static final String BUILD_TIMESTAMP;
+
+  private static final Logger LOGGER = LoggerFactory.getLogger (CAS4Version.class);
+
+  static
+  {
+    String sProjectVersion = null;
+    String sProjectTimestamp = null;
+    final ICommonsMap <String, String> p = PropertiesHelper.loadProperties (new ClassPathResource ("phase4-version.properties"));
+    if (p != null)
+    {
+      sProjectVersion = p.get ("version");
+      sProjectTimestamp = p.get ("timestamp");
+    }
+    if (sProjectVersion == null)
+    {
+      sProjectVersion = "undefined";
+      LOGGER.warn ("Failed to load version number");
+    }
+    BUILD_VERSION = sProjectVersion;
+    if (sProjectTimestamp == null)
+    {
+      sProjectTimestamp = "undefined";
+      LOGGER.warn ("Failed to load timestamp");
+    }
+    BUILD_TIMESTAMP = sProjectTimestamp;
+  }
+
+  private CAS4Version ()
+  {}
+}
---
+++ phase4-lib/src/test/java/com/helger/phase4/CAS4VersionTest.java
@@ -0,0 +1,41 @@
+/**
+ * Copyright (C) 2015-2019 Philip Helger (www.helger.com)
+ * philip[at]helger[dot]com
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *         http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.helger.phase4;
+
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertNotEquals;
+
+import org.junit.Test;
+
+/**
+ * Test class for class {@link CAS4Version}
+ *
+ * @author Philip Helger
+ */
+public final class CAS4VersionTest
+{
+  @Test
+  public void testBasic ()
+  {
+    assertNotEquals ("undefined", CAS4Version.BUILD_VERSION);
+    assertNotEquals ("undefined", CAS4Version.BUILD_TIMESTAMP);
+
+    // Check variable resolution
+    assertFalse (CAS4Version.BUILD_VERSION.contains ("${"));
+    assertFalse (CAS4Version.BUILD_TIMESTAMP.contains ("${"));
+  }
+}
