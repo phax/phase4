39cdbc4ebf2952a1fcea08e4f14ba5c0e8defe60
Philip Helger <philip@helger.com>
2020-06-24 19:35:34 +0200

    Fixed XSDs

--- phase4-lib/src/main/java/com/helger/phase4/marshaller/EEbms3DocumentType.java
+++ phase4-lib/src/main/java/com/helger/phase4/marshaller/EEbms3DocumentType.java
@@ -33,26 +33,34 @@ import com.helger.phase4.ebms3header.Ebms3Messaging;
 import com.helger.phase4.ebms3header.NonRepudiationInformation;
 import com.helger.phase4.soap11.Soap11Envelope;
 import com.helger.phase4.soap12.Soap12Envelope;
+import com.helger.xsds.xlink.CXLink;
+import com.helger.xsds.xml.CXML_XSD;
 import com.helger.xsds.xmldsig.CXMLDSig;
 
 /**
  * Enumeration wizh all known EBMS document types.
- * 
+ *
  * @author Philip Helger
  */
 public enum EEbms3DocumentType implements IJAXBDocumentType
 {
   MESSAGING (Ebms3Messaging.class,
-             new CommonsArrayList <> (CXMLDSig.getXSDResource (), CAS4.XSD_EBBP_SIGNALS, CAS4.XSD_EBMS_HEADER)),
+             new CommonsArrayList <> (CXML_XSD.getXSDResource (),
+                                      CXLink.getXSDResource (),
+                                      CXMLDSig.getXSDResource (),
+                                      CAS4.XSD_EBBP_SIGNALS,
+                                      CAS4.XSD_EBMS_HEADER)),
   NON_REPUDIATION_INFORMATION (NonRepudiationInformation.class,
-                               new CommonsArrayList <> (CXMLDSig.getXSDResource (), CAS4.XSD_EBBP_SIGNALS)),
+                               new CommonsArrayList <> (CXML_XSD.getXSDResource (),
+                                                        CXLink.getXSDResource (),
+                                                        CXMLDSig.getXSDResource (),
+                                                        CAS4.XSD_EBBP_SIGNALS)),
   SOAP_11 (Soap11Envelope.class, new CommonsArrayList <> (CAS4.XSD_SOAP11)),
-  SOAP_12 (Soap12Envelope.class, new CommonsArrayList <> (CAS4.XSD_SOAP12));
+  SOAP_12 (Soap12Envelope.class, new CommonsArrayList <> (CXML_XSD.getXSDResource (), CAS4.XSD_SOAP12));
 
   private final JAXBDocumentType m_aDocType;
 
-  private EEbms3DocumentType (@Nonnull final Class <?> aClass,
-                              @Nonnull final List <? extends ClassPathResource> aXSDPaths)
+  private EEbms3DocumentType (@Nonnull final Class <?> aClass, @Nonnull final List <? extends ClassPathResource> aXSDPaths)
   {
     m_aDocType = new JAXBDocumentType (aClass, aXSDPaths, null);
   }
