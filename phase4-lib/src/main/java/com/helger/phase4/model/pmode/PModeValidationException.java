1fc30a615a55d5e45a032c4a31f6bd9e6d636cb2
Philip Helger <philip@helger.com>
2020-09-20 13:48:07 +0200

    `PModeValidationException` is now a subclass of `Phase4Exception`

--- phase4-lib/src/main/java/com/helger/phase4/model/pmode/PModeValidationException.java
+++ phase4-lib/src/main/java/com/helger/phase4/model/pmode/PModeValidationException.java
@@ -18,13 +18,15 @@ package com.helger.phase4.model.pmode;
 
 import javax.annotation.Nonnull;
 
+import com.helger.phase4.util.Phase4Exception;
+
 /**
  * A special exception thrown in PMode validation.
- * 
+ *
  * @author Philip Helger
  * @since 0.9.6
  */
-public class PModeValidationException extends Exception
+public class PModeValidationException extends Phase4Exception
 {
   public PModeValidationException (@Nonnull final String sMsg)
   {
