13169b9c84af8b8279b1326fcb73a514ee45e20b
Philip Helger <philip@helger.com>
2020-12-11 10:18:20 +0100

    Added "and"

--- phase4-lib/src/main/java/com/helger/phase4/client/IAS4RawResponseConsumer.java
+++ phase4-lib/src/main/java/com/helger/phase4/client/IAS4RawResponseConsumer.java
@@ -17,6 +17,7 @@
 package com.helger.phase4.client;
 
 import javax.annotation.Nonnull;
+import javax.annotation.Nullable;
 
 import com.helger.phase4.util.Phase4Exception;
 
@@ -38,4 +39,46 @@ public interface IAS4RawResponseConsumer
    *         In case of error.
    */
   void handleResponse (@Nonnull AS4ClientSentMessage <byte []> aResponseMsg) throws Phase4Exception;
+
+  /**
+   * Chain this instance with another instance of the same type. This handler is
+   * called first.
+   *
+   * @param rhs
+   *        The handler to chain with. May be <code>null</code>.
+   * @return A non-<code>null</code> response consumer.
+   * @since 0.13.0
+   */
+  @Nonnull
+  default IAS4RawResponseConsumer and (@Nullable final IAS4RawResponseConsumer rhs)
+  {
+    return and (this, rhs);
+  }
+
+  /**
+   * Chain two instances of the same type to a single instance. This first
+   * handler is called first, the second handler is called afterwards. If any of
+   * the two parameters is <code>null</code> no chaining happens.
+   *
+   * @param lhs
+   *        The first handler to invoke. May be <code>null</code>.
+   * @param rhs
+   *        The second handler to invoke. May be <code>null</code>.
+   * @return <code>null</code> if both parameters are <code>null</code>.
+   * @since 0.13.0
+   */
+  @Nullable
+  static IAS4RawResponseConsumer and (@Nullable final IAS4RawResponseConsumer lhs, @Nullable final IAS4RawResponseConsumer rhs)
+  {
+    if (lhs == null)
+      return rhs;
+
+    if (rhs == null)
+      return lhs;
+
+    return x -> {
+      lhs.handleResponse (x);
+      rhs.handleResponse (x);
+    };
+  }
 }
