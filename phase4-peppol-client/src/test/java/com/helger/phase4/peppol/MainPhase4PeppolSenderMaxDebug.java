ef2db783a2a5f26584f906ed3a1c23946b74c4be
Philip Helger <philip@helger.com>
2022-01-12 18:34:55 +0100

    Version update

--- phase4-peppol-client/src/test/java/com/helger/phase4/peppol/MainPhase4PeppolSenderMaxDebug.java
+++ phase4-peppol-client/src/test/java/com/helger/phase4/peppol/MainPhase4PeppolSenderMaxDebug.java
@@ -77,7 +77,7 @@ public final class MainPhase4PeppolSenderMaxDebug
                                   .payload (aPayloadElement)
                                   .smpClient (new SMPClientReadOnly (Phase4PeppolSender.URL_PROVIDER, aReceiverID, ESML.DIGIT_TEST))
                                   .rawResponseConsumer (new AS4RawResponseConsumerWriteToFile ())
-                                  .validationConfiguration (PeppolValidationAUNZ.VID_OPENPEPPOL_BIS3_AUNZ_UBL_INVOICE_105,
+                                  .validationConfiguration (PeppolValidationAUNZ.VID_OPENPEPPOL_BIS3_AUNZ_UBL_INVOICE_106,
                                                             new Phase4PeppolValidatonResultHandler ())
                                   .sendMessageAndCheckForReceipt ();
       LOGGER.info ("Peppol send result: " + eResult);
