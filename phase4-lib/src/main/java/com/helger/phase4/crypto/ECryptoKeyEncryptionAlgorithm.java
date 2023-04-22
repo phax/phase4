af3f43b507b8de686c2143f49faa836302de0d43
Philip Helger <philip@helger.com>
2023-04-22 08:57:24 +0200

    Adopted and extended from PR

--- phase4-lib/src/main/java/com/helger/phase4/crypto/ECryptoKeyEncryptionAlgorithm.java
+++ phase4-lib/src/main/java/com/helger/phase4/crypto/ECryptoKeyEncryptionAlgorithm.java
@@ -1,6 +1,6 @@
 /*
- * Copyright (C) 2023 Gregor Scholtysik (www.soptim.de)
- * gregor[dot]scholtysik[at]soptim[dot]de
+ * Copyright (C) 2015-2023 Philip Helger (www.helger.com)
+ * philip[at]helger[dot]com
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -16,28 +16,57 @@
  */
 package com.helger.phase4.crypto;
 
-import com.helger.commons.annotation.Nonempty;
-import com.helger.commons.id.IHasID;
+import javax.annotation.Nonnull;
+import javax.annotation.Nullable;
+
 import org.apache.wss4j.common.WSS4JConstants;
 
-import javax.annotation.Nonnull;
+import com.helger.commons.annotation.Nonempty;
+import com.helger.commons.id.IHasID;
+import com.helger.commons.lang.EnumHelper;
 
-public enum ECryptoKeyEncryptionAlgorithm implements IHasID<String> {
-    RSA_OAEP_XENC11(WSS4JConstants.KEYTRANSPORT_RSAOAEP_XENC11),
+/**
+ * Enumeration of all key encryption algorithms.
+ *
+ * @author Gregor Scholtysik
+ * @author Philip Helger
+ * @since 2.1.0
+ */
+public enum ECryptoKeyEncryptionAlgorithm implements IHasID <String>
+{
+  RSA15 (WSS4JConstants.KEYTRANSPORT_RSA15),
+  RSA_OAEP (WSS4JConstants.KEYTRANSPORT_RSAOAEP),
+  RSA_OAEP_XENC11 (WSS4JConstants.KEYTRANSPORT_RSAOAEP_XENC11),
 
-    // ECDH-ES KEYWRAP is not supported by WSS4J
-    ECDH_ES_KEYWRAP_AES_128("http://www.w3.org/2001/04/xmlenc#kw-aes128"),
-    ECDH_ES_KEYWRAP_AES_192("http://www.w3.org/2001/04/xmlenc#kw-aes192"),
-    ECDH_ES_KEYWRAP_AES_256("http://www.w3.org/2001/04/xmlenc#kw-aes256");
+  // ECDH-ES KEYWRAP are currently not yet supported by WSS4J 3.0.0
+  ECDH_ES_KEYWRAP_AES_128 ("http://www.w3.org/2001/04/xmlenc#kw-aes128"),
+  ECDH_ES_KEYWRAP_AES_192 ("http://www.w3.org/2001/04/xmlenc#kw-aes192"),
+  ECDH_ES_KEYWRAP_AES_256 ("http://www.w3.org/2001/04/xmlenc#kw-aes256");
 
-    private final String m_sID;
+  private final String m_sID;
 
-    ECryptoKeyEncryptionAlgorithm (@Nonnull @Nonempty final String sID) {
-        this.m_sID = sID;
-    }
+  ECryptoKeyEncryptionAlgorithm (@Nonnull @Nonempty final String sID)
+  {
+    m_sID = sID;
+  }
 
-    @Override
-    public String getID () {
-        return m_sID;
-    }
+  @Nonnull
+  @Nonempty
+  public String getID ()
+  {
+    return m_sID;
+  }
+
+  @Nullable
+  public static ECryptoKeyEncryptionAlgorithm getFromIDOrNull (@Nullable final String sID)
+  {
+    return EnumHelper.getFromIDOrNull (ECryptoKeyEncryptionAlgorithm.class, sID);
+  }
+
+  @Nullable
+  public static ECryptoKeyEncryptionAlgorithm getFromIDOrDefault (@Nullable final String sID,
+                                                                  @Nullable final ECryptoKeyEncryptionAlgorithm eDefault)
+  {
+    return EnumHelper.getFromIDOrDefault (ECryptoKeyEncryptionAlgorithm.class, sID, eDefault);
+  }
 }
