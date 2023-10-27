36a83cb84497c5586e1e1a88916187994cff981d
Philip Helger <philip@helger.com>
2023-10-27 12:47:22 +0200

    Fixed typo

--- phase4-test/src/test/java/com/helger/phase4/server/external/IHolodeckTests.java
+++ phase4-test/src/test/java/com/helger/phase4/server/external/IHolodeckTests.java
@@ -19,7 +19,7 @@ package com.helger.phase4.server.external;
 /**
  * An interface is needed to use JUnits Category feature. Every test that is
  * tagged with IHolodeckTests, should be green when run with a holodeck instance
- * with esens-connector as plugin. For PH-AS4 everything should be green!
+ * with esens-connector as plugin. For phase4 everything should be green!
  *
  * @author bayerlma
  */
