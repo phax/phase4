310fa885830732d52705c94e880841eb82a522e5
Philip Helger <philip@helger.com>
2024-07-31 09:57:17 +0200

    Added a chnageV3

--- phase4-peppol-servlet/src/main/java/com/helger/phase4/peppol/servlet/Phase4PeppolServletConfiguration.java
+++ phase4-peppol-servlet/src/main/java/com/helger/phase4/peppol/servlet/Phase4PeppolServletConfiguration.java
@@ -29,6 +29,7 @@ import com.helger.commons.ValueEnforcer;
 import com.helger.commons.string.StringHelper;
 import com.helger.peppol.sbdh.read.PeppolSBDHDocumentReader;
 import com.helger.phase4.CAS4;
+import com.helger.phase4.v3.ChangeV3;
 import com.helger.smpclient.peppol.ISMPServiceMetadataProvider;
 import com.helger.smpclient.peppol.PeppolWildcardSelector;
 import com.helger.smpclient.peppol.PeppolWildcardSelector.EMode;
@@ -43,6 +44,7 @@ import com.helger.smpclient.peppol.PeppolWildcardSelector.EMode;
  * @author Philip Helger
  */
 @NotThreadSafe
+@ChangeV3 ("Rename to Phase4PeppolGlobalServletConfiguration")
 public final class Phase4PeppolServletConfiguration
 {
   public static final boolean DEFAULT_RECEIVER_CHECK_ENABLED = true;
