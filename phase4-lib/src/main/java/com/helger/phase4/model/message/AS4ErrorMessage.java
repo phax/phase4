53fdbe903f1f3dd39fac3626c2ed38571ebeb060
Philip Helger <philip@helger.com>
2025-07-02 16:15:26 +0200

    Fixed typo

--- phase4-lib/src/main/java/com/helger/phase4/model/message/AS4ErrorMessage.java
+++ phase4-lib/src/main/java/com/helger/phase4/model/message/AS4ErrorMessage.java
@@ -29,11 +29,10 @@ import com.helger.phase4.model.ESoapVersion;
 
 /**
  * AS4 error message.<br>
- * An Error does not have to be associated with a P-Mode when it is
- * synchronously send back. Only when Errors need to be send async a P-Mode is
- * needed to find the URL where to send to. <br>
- * Errors do not need to be signed, just because problems like these can occur
- * where a gateway doesn’t know how to sign.
+ * An Error does not have to be associated with a P-Mode when it is synchronously send back. Only
+ * when Errors need to be send async a P-Mode is needed to find the URL where to send to. <br>
+ * Errors do not need to be signed, just because problems like these can occur where a gateway
+ * doesn't know how to sign.
  *
  * @author Philip Helger
  */
@@ -56,8 +55,7 @@ public class AS4ErrorMessage extends AbstractAS4Message <AS4ErrorMessage>
   }
 
   /**
-   * @return The {@link Ebms3SignalMessage} passed in the constructor. Never
-   *         <code>null</code>.
+   * @return The {@link Ebms3SignalMessage} passed in the constructor. Never <code>null</code>.
    */
   @Nonnull
   public final Ebms3SignalMessage getEbms3SignalMessage ()
