dd980a42f4201b6901733f9085e84713c289acbe
Philip Helger <philip@helger.com>
2025-07-10 20:45:50 +0200

    Prepared for release

--- phase4-peppol-client/src/test/java/com/helger/phase4/peppol/receivers/MainPhase4PeppolSenderQvaliaUBL.java
+++ phase4-peppol-client/src/test/java/com/helger/phase4/peppol/receivers/MainPhase4PeppolSenderQvaliaUBL.java
@@ -105,7 +105,7 @@ public final class MainPhase4PeppolSenderQvaliaUBL
                                                                      aReceiverID,
                                                                      ESML.DIGIT_TEST))
                                   .validationRegistry (aVESRegistry)
-                                  .validationConfiguration (EN16931Validation.VID_UBL_INVOICE_1314_1,
+                                  .validationConfiguration (EN16931Validation.VID_UBL_INVOICE_1314_2,
                                                             new Phase4PeppolValidatonResultHandler ())
                                   .buildMessageCallback (aBuildMessageCallback)
                                   .sendMessageAndCheckForReceipt ();
