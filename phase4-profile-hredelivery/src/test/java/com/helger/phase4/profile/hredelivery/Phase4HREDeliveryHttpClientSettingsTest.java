3c653d5212449bf67e99a601cf0c2fe3e4d7be4d
Philip Helger <philip@helger.com>
2025-09-02 16:43:53 +0200

    Started AS4 profile for HR eDelivery

---
+++ phase4-profile-hredelivery/src/test/java/com/helger/phase4/profile/hredelivery/Phase4HREDeliveryHttpClientSettingsTest.java
@@ -0,0 +1,33 @@
+/*
+ * Copyright (C) 2025 Philip Helger (www.helger.com)
+ * philip[at]helger[dot]com
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *         http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.helger.phase4.profile.hredelivery;
+
+import org.junit.Test;
+
+/**
+ * Test class for class {@link Phase4HREDeliveryHttpClientSettings}
+ *
+ * @author Philip Helger
+ */
+public class Phase4HREDeliveryHttpClientSettingsTest
+{
+  @Test
+  public void testBasic ()
+  {
+    new Phase4HREDeliveryHttpClientSettings ();
+  }
+}
