0c7655fbe71505e7ca227b23a8bc7020663e9ed7
Philip Helger <philip@helger.com>
2026-03-19 07:47:00 +0100

    Added additional crypt parameter configuration

---
+++ phase4-lib/src/main/java/com/helger/phase4/crypto/ECryptoKeyDerivationMethod.java
@@ -0,0 +1,67 @@
+/*
+ * Copyright (C) 2015-2026 Philip Helger (www.helger.com)
+ * philip[at]helger[dot]com
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *         http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.helger.phase4.crypto;
+
+import org.apache.wss4j.common.WSS4JConstants;
+import org.jspecify.annotations.NonNull;
+import org.jspecify.annotations.Nullable;
+
+import com.helger.annotation.Nonempty;
+import com.helger.base.id.IHasID;
+import com.helger.base.lang.EnumHelper;
+
+/**
+ * Enumeration of key derivation functions used in key agreement schemes. The key derivation
+ * function takes the shared secret from key agreement and derives the actual key encryption key.
+ *
+ * @author Philip Helger
+ * @since 4.4.0
+ */
+public enum ECryptoKeyDerivationMethod implements IHasID <String>
+{
+  /** ConcatKDF (NIST SP 800-56A) */
+  CONCAT_KDF (WSS4JConstants.KEYDERIVATION_CONCATKDF),
+  /** HKDF (RFC 5869) - eDelivery AS4 2.0 */
+  HKDF (WSS4JConstants.KEYDERIVATION_HKDF);
+
+  private final String m_sID;
+
+  ECryptoKeyDerivationMethod (@NonNull @Nonempty final String sID)
+  {
+    m_sID = sID;
+  }
+
+  @NonNull
+  @Nonempty
+  public String getID ()
+  {
+    return m_sID;
+  }
+
+  @Nullable
+  public static ECryptoKeyDerivationMethod getFromIDOrNull (@Nullable final String sID)
+  {
+    return EnumHelper.getFromIDOrNull (ECryptoKeyDerivationMethod.class, sID);
+  }
+
+  @Nullable
+  public static ECryptoKeyDerivationMethod getFromIDOrDefault (@Nullable final String sID,
+                                                               @Nullable final ECryptoKeyDerivationMethod eDefault)
+  {
+    return EnumHelper.getFromIDOrDefault (ECryptoKeyDerivationMethod.class, sID, eDefault);
+  }
+}
