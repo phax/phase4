f1fe94c9ad78424257f7c748c6f44158bad75e89
Philip Helger <philip@helger.com>
2026-08-30 15:17:16 +0200

    Version bump

--- phase4-profile-bdew/src/test/java/com/helger/phase4/profile/bdew/BDEWPModeTemplateResolutionTest.java
+++ phase4-profile-bdew/src/test/java/com/helger/phase4/profile/bdew/BDEWPModeTemplateResolutionTest.java
@@ -1,6 +1,6 @@
 /*
- * Copyright (C) 2015-2026 Philip Helger (www.helger.com)
- * philip[at]helger[dot]com
+ * Copyright (C) 2023-2026 Gregor Scholtysik (www.soptim.de)
+ * gregor[dot]scholtysik[at]soptim[dot]de
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
